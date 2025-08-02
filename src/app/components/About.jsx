f237fb48ad0853acf8c7620065d0d626027b382b
nobabsarkar <nobabsarkar2016@gmail.com>
2025-08-02 22:22:34 +0600

    change code

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -7,7 +7,7 @@ import { infoList } from "../../../assets/assets";
 const About = () => {
   return (
     <div id="about" className="w-full px-[12%] py-10 scroll-mt-20 ">
-      <h4 className="text-center mb-2 text-lg font-Ovo">Introduction</h4>
+      <h4 className="text-center mb-2 text-lg font-Ovo">My Introduction</h4>
       <h2 className="text-center text-5xl font-Ovo">About me</h2>
 
       <div className="flex w-full flex-col lg:flex-row items-center gap-20 my-20">
