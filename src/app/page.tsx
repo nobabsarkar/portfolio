f361c721acded9217159c1327bc747da514440a6
Nobab Sarkar <108366419+nobabsarkar@users.noreply.github.com>
2025-08-19 23:32:39 +0600

    comment portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
 
+// this is navbar header about call page
 export default function Home() {
   return (
     <div>
